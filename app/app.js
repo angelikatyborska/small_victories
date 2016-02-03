b7853851d5735e41d7b10a86e6cbb547061b5f7e
Angelika Tyborska <atyborska93@gmail.com>
2016-02-03 13:23:28 +0100

    Use angular-moment

--- app/app.js
+++ app/app.js
@@ -1,7 +1,12 @@
 'use strict';
 
+require('moment');
+var angularMoment = require('angular-moment');
 var angular = require('angular');
-var app = angular.module('smallVictories', []);
+var app = angular.module('smallVictories', ['angularMoment']);
+app.run(function(amMoment) {
+  amMoment.changeLocale('en');
+});
 
 require('./services');
 require('./controllers');
