deb77c60eaf9ee9c1c55c8e93d44637552f8597f
Angelika Tyborska <atyborska93@gmail.com>
2016-02-08 22:19:59 +0100

    Allow CORS

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "small-victories-api.herokuapp.com");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static('public'));
 
 var port = process.env.PORT || 5000;
